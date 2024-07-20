2f0c6138edc0051e86a69682bb75a6817428405f
Pranjal Saxena <pranjalpro12@gmail.com>
2024-07-20 15:52:09 +0530

    Update and rename GreedyAlgo.java

--- GreedyAlgo.java
+++ GreedyAlgo.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class AlphaGreedyAlgo {
+public class GreedyAlgo {
     public static void main(String args[]) {
         int start[] = { 1, 3, 0, 5, 8, 5 };
         int end[] = { 2, 4, 6, 7, 9, 9 };
